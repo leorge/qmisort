a1714a296816f0f96b4b553877bdb2f2ea6c9a4f
leorge <qmisort@gmail.com>
2015-02-20 21:17:46 +0900

    iqsort() --> pqsort(), and add iqsort()

--- index.c
+++ index.c
@@ -89,3 +89,13 @@ void pqsort(void **base, size_t nmemb, int (*compare)(const void *, const void *
 	comp = compare;
 	qsort((void *)base, nmemb, sizeof(void *), comp_idx);
 }
+
+void iqsort(void *base, size_t nmemb, size_t size, int (*compare)(const void *, const void *)) {
+	if (nmemb <= 1) return;
+	void **idxtbl = make_index(base, nmemb, size);
+	if (idxtbl != NULL) {
+		pqsort(idxtbl, nmemb, compare);
+		unindex(base, idxtbl, nmemb, size);
+		free(idxtbl);
+	}
+}
