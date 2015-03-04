77182afdbd66bee6e3fdfa16a4dd2d8f1b9700a8
leorge <qmisort@gmail.com>
2015-03-04 16:22:49 +0900

    Add a script to exhibit crash JavaX

---
+++ CrashX.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -v
+
+# Show version
+java -version
+perl --version
+
+# Download from princeton.edu and compile
+wget http://introcs.cs.princeton.edu/java/stdlib/stdlib.jar
+wget http://algs4.cs.princeton.edu/23quicksort/QuickX.java
+jar xf stdlib.jar 
+javac -cp :. QuickX.java 
+
+# Download from Github and test scripts
+wget https://github.com/leorge/QuickJava/raw/master/KillQuickX.pl
+wget https://github.com/leorge/QuickJava/raw/master/QuickX.pl
+chmod +x *.pl
+./KillQuickX.pl 60 | xargs ./QuickX.pl 
+
+# Crash
+./KillQuickX.pl 40000 | java -cp :. QuickX 2>&1 | head 
+
