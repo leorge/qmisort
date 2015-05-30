0d0b5651f6d03e31888408d6cfc5dd726acb5cd2
leorge <qmisort@gmail.com>
2015-05-30 14:05:55 +0900

    Add insert_pointer

--- insert_sort.c
+++ insert_sort.c
@@ -11,6 +11,33 @@
 
 static size_t   length;
 
+/* pointer sorting */
+void insert_pointer(void **base, size_t nmemb, int (*compare)(const void *, const void *)) {
+    if (nmemb <= 1) return;
+#ifdef DEBUG
+    if (trace_level >= TRACE_DUMP) dump_pointer("insert_pointer() start in " __FILE__, base, nmemb);
+#endif
+    void *pivot, **p1, **p2;
+    for (size_t i = 1; i < nmemb; i++) {
+        p2 = base + i;
+#ifdef DEBUG
+        if (trace_level >= TRACE_DUMP) {
+        	fprintf(OUT, "insert %s", dump_data(*p2));
+        	dump_pointer("", base, i);
+        }
+#endif
+        pivot = *p2;
+        while (p2 > base) {
+        	if (compare(*(p1 = p2 - 1), pivot) <= 0) break;
+        	*p2 = *p1; p2 = p1;
+        }
+        *p2 = pivot;
+    }
+#ifdef DEBUG
+    if (trace_level >= TRACE_DUMP) dump_pointer("insert_pointer() done.", base, nmemb);
+#endif
+}
+
 /* array sorting */
 static void copy(void *dst, const void *src)
 {
@@ -30,21 +57,20 @@ void insert_sort(void *base, size_t nmemb, size_t size, int (*compare)(const voi
 #define first   ((char *)base)
     char pivot[size];
     for (size_t i = 1; i < nmemb; i++) {
-        char    *p, *hole = first + i * size;
+        char    *p1, *p2 = first + i * size;
 #ifdef DEBUG
         if (trace_level >= TRACE_DUMP) {
-        	char msg[20 + size]; sprintf(msg, "insert %s", hole);
+        	char msg[20 + size]; sprintf(msg, "insert %s", p2);
         	dump_array(msg, base, i, size);
         }
         qsort_called++;	// loop count
 #endif
-        copy(pivot, hole);  // make a hole
-        while (hole > first) {
-            if (compare(p = hole - size, pivot) <= 0) break;
-            copy(hole, p);  // move a hole
-            hole = p;
+        copy(pivot, p2);
+        while (p2 > first) {
+            if (compare(p1 = p2 - size, pivot) <= 0) break;
+            copy(p2, p1); p2 = p1;
         }
-        copy(hole, pivot);  // bury the last hole
+        copy(p2, pivot);
     }
 #ifdef DEBUG
     if (trace_level >= TRACE_DUMP) dump_array("insert_linear() done.", base, nmemb, size);
