f59e5a4444d596f8799391f987d0523bddc1ff84
leorge <qmisort@gmail.com>
2015-05-31 21:01:47 +0900

    Add a shell scritp to evaluate the best small_boundary

---
+++ lopt.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Search the best small_boundary.
+#
+N=100000
+for Z in 16 64 256; do
+    for l in `seq 2 32`; do 
+        echo $Z $l
+        for i in `seq 1 10`; do
+            src/random.awk $N | Release/Sort -N $N -Z $Z -l $l -E
+        done
+        echo ""
+    done
+done | grep -v qsort | tee tmp$$
+
+awk -f - tmp$$ << 'EOF'
+NF==2 {z=$1; l=$2} # element size, small boundary
+NF>3 && $8<3 {     # Stdev is less than 3%
+    a[z,l] += $4;  # elapsed time in micro seconds
+}
+END {
+    for (l=2;l<=32;l++) printf "\t%d", l;
+    print "";
+    for (z=16; z<=256; z*=4) {  # 16, 64, 256
+        printf "%d", z;
+        for (l=2;l<=32;l++) printf "\t%d", a[z,l];
+        print "";
+    }
+}
+EOF
+
