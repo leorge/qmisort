faa1043ead61f76a0ef20329f89d8fde9d595d01
leorge <qmisort@gmail.com>
2015-07-04 16:58:25 +0900

    Perl script to genearte a killer data if a pivot is the middle element for hole scheme quick sort.

---
+++ hole_middle.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+#
+# generate a bad data for hybrid_array() random is 0.5.
+#
+use strict;
+use warnings;
+$#ARGV < 0 && die "Usage : $0 count\n";
+my $n = shift(@ARGV);   # count
+$n < 1 && die "count > 0\n";
+my $fmt = sprintf("%%0%dd\n", length($n - 1));
+#warn "\$n = $n\t\$fmt = $fmt\n";
+my @list = (--$n);
+while ($n--) {
+    my @center = splice(@list, ($#list - 1) / 2, 1, $n);
+    my @last = splice(@list, $#list, 1, @center);
+    unshift @list, @last;
+}
+foreach my $i (@list) {
+    printf $fmt, $i;
+}
