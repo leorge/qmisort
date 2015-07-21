3612553a00640347ece8d19ab109f1aabe01c62a
leorge <qmisort@gmail.com>
2015-07-22 07:48:03 +0900

    retrograde

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,7 +1,7 @@
 /*
  * bubble_sort.c
  *
- *  Bubble sort without swaps.
+ *  Improved bubble sort that uses anchor pointer instead of exchanged flag.
  *
  *  Created on: 2015/05/23
  *      Author: leo
@@ -9,57 +9,38 @@
 
 #include "sort.h"
 
-static size_t   length;
-
 /* pointer sorting  */
 void bubble_pointer(void **base, size_t nmemb, int (*compare)(const void *, const void *)) {
     if (nmemb <= 1) return;
 #ifdef DEBUG
     if (trace_level >= TRACE_DUMP) dump_pointer("bubble_pointer() start in " __FILE__, base, nmemb);
 #endif
-    void *pivot;
-    void **first = base;
-    void **last = first + nmemb - 1;
+    void **last = base + nmemb - 1;
     do {
-        void **p1, **p2, **anchor = first + 1;
+        void **p1, **p2, *tmp, **anchor = base + 1;
 #ifdef DEBUG
-        if (trace_level >= TRACE_DUMP) dump_pointer("bubble_pointer()", base, last - first + 1);
+        if (trace_level >= TRACE_DUMP) dump_pointer("bubble_pointer()", base, last - base + 1);
 #endif
-        for (p1 = first; (p2 = p1 + 1) <= last; p1++) {
-            if (compare(*p1, *p2) > 0) {
-                pivot = *p1; *p1 = *p2;
-                for (p1 = p2; (p2 = p1 + 1) <= last; p1++) {
-                    if (compare(*p2, pivot) > 0) break;
-                    *p1 = *p2;
-                }
-                *(anchor = p1) = pivot;
+        for (p1 = base; p1 < last; p1++) {
+            if (compare(*p1, *(p2 = p1 + 1)) > 0) {
+            	tmp = *p1;
+            	*p1 = *p2;
+            	*(anchor = p2) = tmp;
             }
         }
         last = anchor - 1;
-    } while (first < last);
+    } while (base < last);
 #ifdef DEBUG
     if (trace_level >= TRACE_DUMP) dump_pointer("bubble_pointer() done.", base, nmemb);
 #endif
 }
 
 /* array sorting */
-static void copy(void *dst, const void *src)
-{
-#ifdef  DEBUG
-    qsort_moved++;
-    if (trace_level >= TRACE_MOVE) fprintf(OUT, "copy(dst = %s, src = %s)\n", dump_data(dst), dump_data(src));
-#endif
-    memcpy(dst, src, length); /* restore an elements  */
-}
 
 void bubble_sort(void *base, size_t nmemb, size_t size, int (*compare)(const void *, const void *)) {
     if (nmemb <= 1) return;
-    char pivot[size];
-#ifdef DEBUG
-    *pivot = 0;
-#endif
-    length = size;
-    char *first = base;
+    char tmp[size];
+#define first   ((char *)base)
     char *last = first + (nmemb - 1) * size;
     do {
         char *p1, *p2, *anchor = first + size;
@@ -67,22 +48,18 @@ void bubble_sort(void *base, size_t nmemb, size_t size, int (*compare)(const voi
         if (trace_level >= TRACE_DUMP) dump_array("search", first, (last - first) / size + 1, size);
         qsort_called++; // loop count
 #endif
-        for (p1 = first; (p2 = p1 + size) <= last; p1 += size) {    // search a reverse order pair
-            if (compare(p1, p2) > 0) {  // found
+        for (p1 = first; p1 < last; p1 += size) {   // search a reverse order pair
+            if (compare(p1, p2 = p1 + size) > 0) {  // found
 #ifdef DEBUG
-                if (trace_level >= TRACE_DUMP) fprintf(OUT, "revese order %s --> %s\n", p1, p2);
+				qsort_moved += 3;
+				if (trace_level >= TRACE_MOVE) fprintf(OUT, "%s <--> %s\n", dump_data(p1), dump_data(p2));
 #endif
-                copy(pivot, p1);     // pick up the greater element
-                copy(p1, p2);        // shift the lesser element left
-                for (p1 = p2; (p2 = p1 + size) <= last; p1 += size) {   // search a more greater element
-                    if (compare(p2, pivot) > 0) break;  // found
-                    copy(p1, p2);   // shift left
-                }
-                copy(anchor = p1, pivot);   // insert before the more greater element
+            	memcpy(tmp, p1, size);
+            	memcpy(p1, p2, size);
+            	memcpy(anchor = p2, tmp, size);
             }
         }
-        // array[anchor..last] are sorted
-        last = anchor - size;   // array[first..last] are not sorted
+        last = anchor - size;
     } while (first < last);
 #ifdef DEBUG
         if (trace_level >= TRACE_DUMP) dump_array("bubble_sort() done.", base, nmemb, size);
