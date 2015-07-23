cb2d7a84aa9652f6ce9ff6915285e1df8e0c32e0
leorge <qmisort@gmail.com>
2015-07-23 15:55:22 +0900

    get rid of bubble_pointer().

--- bubble_sort.c
+++ bubble_sort.c
@@ -9,34 +9,6 @@
 
 #include "sort.h"
 
-/* pointer sorting  */
-void bubble_pointer(void **base, size_t nmemb, int (*compare)(const void *, const void *)) {
-    if (nmemb <= 1) return;
-#ifdef DEBUG
-    if (trace_level >= TRACE_DUMP) dump_pointer("bubble_pointer() start in " __FILE__, base, nmemb);
-#endif
-    void **last = base + nmemb - 1;
-    do {
-        void **p1, **p2, *tmp, **anchor = base + 1;
-#ifdef DEBUG
-        if (trace_level >= TRACE_DUMP) dump_pointer("bubble_pointer()", base, last - base + 1);
-#endif
-        for (p1 = base; p1 < last; p1++) {
-            if (compare(*p1, *(p2 = p1 + 1)) > 0) {
-            	tmp = *p1;
-            	*p1 = *p2;
-            	*(anchor = p2) = tmp;
-            }
-        }
-        last = anchor - 1;
-    } while (base < last);
-#ifdef DEBUG
-    if (trace_level >= TRACE_DUMP) dump_pointer("bubble_pointer() done.", base, nmemb);
-#endif
-}
-
-/* array sorting */
-
 void bubble_sort(void *base, size_t nmemb, size_t size, int (*compare)(const void *, const void *)) {
     if (nmemb <= 1) return;
     char tmp[size];
@@ -48,7 +20,7 @@ void bubble_sort(void *base, size_t nmemb, size_t size, int (*compare)(const voi
         if (trace_level >= TRACE_DUMP) dump_array("search", first, (last - first) / size + 1, size);
         qsort_called++; // loop count
 #endif
-        for (p1 = first; p1 < last; p1 += size) {   // search a reverse order pair
+        for (p1 = first; p1 < last; p1 = p2) {   // search a reverse order pair
             if (compare(p1, p2 = p1 + size) > 0) {  // found
 #ifdef DEBUG
 				qsort_moved += 3;
