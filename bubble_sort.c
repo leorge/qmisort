11349b3f7a8f8ab1c3a7bc57c66aa807366e08e5
leorge <qmisort@gmail.com>
2015-08-17 15:07:25 +0900

    refactoring a bit

--- bubble_sort.c
+++ bubble_sort.c
@@ -11,26 +11,27 @@
 
 void bubble_sort(void **base, size_t nmemb, int (*compare)(const void *, const void *)) {
     if (nmemb <= 1) return;
-    void *tmp;
-    void **first = base;
-    void **last = first + (nmemb - 1);
-    do {
-        void **p1, **p2, **anchor = first + 1;
 #ifdef DEBUG
-    dump_pointer("search", first, last - first + 1);
+    dump_pointer("bubble_sort() start in " __FILE__, base, nmemb);
+#endif
+    void **last = base + (nmemb - 1);
+    do {
+        void **p1, **p2, **anchor = base + 1;
+#ifdef DEBUG
+    dump_pointer("search", base, last - base + 1);
         qsort_called++; // loop count
 #endif
-        for (p1 = first; p1 < last; p1 = p2) {   // search a reverse order pair
+        for (p1 = base; p1 < last; p1 = p2) {   // search a reverse order pair
             if (compare(*p1, *(p2 = p1 + 1)) > 0) {  // found
 #ifdef DEBUG
 				if (trace_level >= TRACE_MOVE) fprintf(OUT, "%s <--> %s\n", dump_data(*p1), dump_data(*p2));
 #endif
-				tmp = *p1; *p1 = *p2; *p2 = tmp;	// swap
+				void *tmp = *p1; *p1 = *p2; *p2 = tmp;	// swap
 				anchor = p2;
             }
         }
         last = anchor - 1;
-    } while (first < last);
+    } while (base < last);
 #ifdef DEBUG
     dump_pointer("bubble_sort() done.", base, nmemb);
 #endif
