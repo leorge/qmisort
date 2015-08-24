4d8281eefcc40ff67d525afa8e48abbb358c6518
leorge <qmisort@gmail.com>
2015-08-24 14:57:16 +0900

    Add comment

--- psort.sh
+++ psort.sh
@@ -2,28 +2,43 @@
 #
 # Get comparisons of pointer sorting in various N.
 #
+#   -P : Pointer sorting
+#       m - Merge sort.
+#       q - Quicksort. pivot is a middle element.
+#       i - Linear insertion sort.
+#       b - Binary insertion sort.
+#       L - sheLL sort
+#       h - Heap sort. heapfy by sift up.
+#       H - Heap sort. heapfy by sift down.
+#       B - Bubble sort.
+#       C - Comb sort.
+#       u - step Up sort.
+#       p - steP sort. step up and down sort.
+#
 if [ $# -eq 0 ]; then
-	echo $0 numbers
-	exit;
+    echo $0 numbers
+    exit;
 fi
+
 for N in $*; do
-    src/random.awk $N | Debug/Sort -N $N -l $N -P mqibLhHBCup -V 1
+    src/random.awk $N | Debug/Sort -N $N -P mqibLhHBCup -V 1
 done > tmp$$
 
 awk -f - tmp$$ <<'EOF'
 BEGIN {N = "N"}
 /^arguments/ {N = N "\t" $4; next}
 {
-	name = $1; sub(/\(\*\)/, "", name);
-	if (! comp[name]) {
-		names = names FS name;
-		comp[name] = name;
-	}
-	comp[name] = comp[name] "\t" $10;
+    name = $1; sub(/\(\*\)/, "", name);
+    if (! comp[name]) {
+        names = names FS name;
+        comp[name] = name;
+    }
+    comp[name] = comp[name] "\t" $10;
 }
 END {
-	print N;
-	N = split(names, y);
-	for (i = 1; i <= N; i++) print comp[y[i]];
+    print N;
+    N = split(names, y);
+    for (i = 1; i <= N; i++) print comp[y[i]];
 }
 EOF
+rm tmp$$
