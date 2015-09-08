f8e83031f4a4f642666167de5327d4d64a390863
leorge <qmisort@gmail.com>
2015-09-08 14:49:09 +0900

    correct misspelling

--- quick_hole.c
+++ quick_hole.c
@@ -93,7 +93,7 @@ static void psort(void **base, size_t nmemb) {
 #endif
 	void **first = base;
     void **last = first + (nmemb - 1);  // point a last element
-    void **hole = first + (nmemb >> 1);	// meddle element
+    void **hole = first + (nmemb >> 1);	// middle element
     pivot = *hole;
 #ifdef  DEBUG
     if (trace_level >= TRACE_DUMP) fprintf(OUT, "pivot <-- %s [%ld]\n", dump_data(pivot), hole - first);
