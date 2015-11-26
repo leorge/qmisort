00d9097e5fe36bd48073c68e36703ccc24244f28
leorge <qmisort@gmail.com>
2015-11-26 12:41:34 +0900

    add test sequences, etc.

--- CrashX.sh
+++ CrashX.sh
@@ -6,7 +6,7 @@ wget http://algs4.cs.princeton.edu/23quicksort/QuickX.java
 javac -cp .:stdlib.jar QuickX.java
 
 # Download a killer data generator from Github
-wget https://github.com/leorge/Sort/qmisort/blob/master/KillQuickX.pl
+wget https://github.com/leorge/qmisort/raw/master/KillQuickX.pl
 chmod +x *.pl
 
 # Show versions
@@ -15,9 +15,13 @@ java -version
 perl --version
 
 # test
+seq -w 1 16 | shuf | xargs echo
+
 seq -w 1 16 | shuf | java -cp .:stdlib.jar QuickX | xargs echo
+
 ./KillQuickX.pl 50 | xargs echo
 
 # Crash
 ./KillQuickX.pl 40000 | java -cp .:stdlib.jar QuickX 2>&1 | head 
 
+nl -ba QuickX.java | grep 94
