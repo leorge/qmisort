2dc84c140fe195c68adacb16dbb20a8e2d25c364
leorge <qmisort@gmail.com>
2015-11-26 20:16:14 +0900

    modified

--- CrashX.sh
+++ CrashX.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -v
 
-# Download java programs from princeton.edu & compile
+# Download java programs from princeton.edu
 wget http://introcs.cs.princeton.edu/java/stdlib/stdlib.jar
 wget http://algs4.cs.princeton.edu/23quicksort/QuickX.java
-javac -cp .:stdlib.jar QuickX.java
 
 # Download a killer data generator from Github
 wget https://github.com/leorge/qmisort/raw/master/KillQuickX.pl
@@ -14,14 +13,16 @@ java -version
 
 perl --version
 
-# test
-seq -w 1 16 | shuf | xargs echo
+# compile & test
+javac -cp .:stdlib.jar QuickX.java
+
+seq -w 1 16 | shuf | xargs echo  # sample data
 
 seq -w 1 16 | shuf | java -cp .:stdlib.jar QuickX | xargs echo
 
-./KillQuickX.pl 50 | xargs echo
+# generate killer data
+./KillQuickX.pl 50 | xargs echo  # sample
 
-# Crash
 ./KillQuickX.pl 40000 | java -cp .:stdlib.jar QuickX 2>&1 | head 
 
 nl -ba QuickX.java | grep 94
