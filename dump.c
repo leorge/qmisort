47dd1c518bd66e32dac7bdf61102e3889b659083
leorge <qmisort@gmail.com>
2015-11-29 04:59:56 +0900

    Add dump_copy()

--- dump.c
+++ dump.c
@@ -16,6 +16,12 @@ const char *dump_data(const void *data) {
     return ispointer? *(const char **)data: data ;
 }
 
+void dump_copy(void *dst, const void *src)
+{
+qsort_moved++;
+if (trace_level >= TRACE_MOVE) fprintf(OUT, "copy(dst = %s, src = %s)\n", dump_data(dst), dump_data(src));
+}
+
 void dump_array(const char *msg, const void *base, size_t nmemb, size_t size)
 {
     if (trace_level < TRACE_DUMP) return;
