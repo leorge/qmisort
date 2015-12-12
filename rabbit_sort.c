e7eacca55e7e8323e6fa9273eb19cdb6f999b9a3
leorge <qmisort@gmail.com>
2015-12-12 11:16:01 +0900

    TAB --> 4 spaces

--- rabbit_sort.c
+++ rabbit_sort.c
@@ -1,9 +1,7 @@
 /*
  * rabbit_sort.c
  *
- *  Rabbit sort. Unstable.
- *
- *  	Variant of bi-directional bubble sort
+ *  Rabbit sort is variant of bi-directional bubble sort. Unstable.
  *
  *  Created on: 2015/05/23
  *      Author: leo
@@ -23,25 +21,25 @@ void rabbit_sort(void **base, size_t nmemb, int (*compare)(const void *, const v
         qsort_called++; // corresponds to recursive calls
 #endif
         char **rabbit, *tmp;
-        char **hole = NULL;		// a rabbit hole
-        char **anchor = NULL;	// exchanged position
+        char **hole = NULL;     // a rabbit hole
+        char **anchor = NULL;   // exchanged position
         for (char **p = rabbit = first; p++ < last;) {
-            int chk = compare(*p, *rabbit);	// compare the size
-            if (chk < 0) hole = p;	// a smaller rabbit is in a hole.
-            else {	// found a not smaller rabbit
-            	if (hole != NULL) {
+            int chk = compare(*p, *rabbit); // compare the size
+            if (chk < 0) hole = p;  // a smaller rabbit is in a hole.
+            else {  // found a not smaller rabbit
+                if (hole != NULL) {
                     tmp = *hole; *hole = *rabbit; *rabbit = tmp;  // push out a smaller rabbit.
                     anchor = hole; hole = NULL; rabbit = p;
-            	}
-            	else if (chk > 0) rabbit = p;	// a bigger rabbit.
+                }
+                else if (chk > 0) rabbit = p;   // a bigger rabbit.
             }
         }
-        if (hole != NULL) {	// The biggest rabbit is roaming.
-        	assert(hole == last);
-        	tmp = *hole; *hole = *rabbit; *rabbit = tmp;
+        if (hole != NULL) { // The biggest rabbit is roaming.
+            assert(hole == last);
+            tmp = *hole; *hole = *rabbit; *rabbit = tmp;
             anchor = hole; hole = NULL;
         }
-        else if (anchor == NULL) break;	// no changed
+        else if (anchor == NULL) break; // no changed
         last = anchor - 1;
 #ifdef DEBUG
         dump_pointer("Search a smaller rabbit.", first, last - first + 1);
@@ -51,19 +49,19 @@ void rabbit_sort(void **base, size_t nmemb, int (*compare)(const void *, const v
             int chk = compare(*p, *rabbit);
             if (chk > 0) hole = p;
             else {
-            	if (hole != NULL) {
-            		tmp = *hole; *hole = *rabbit; *rabbit = tmp;
+                if (hole != NULL) {
+                    tmp = *hole; *hole = *rabbit; *rabbit = tmp;
                     anchor = hole; hole = NULL; rabbit = p;
-            	}
-            	else if (chk < 0) rabbit = p;
+                }
+                else if (chk < 0) rabbit = p;
             }
         }
         if (hole != NULL) {
-        	assert(hole == first);
-        	tmp = *hole; *hole = *rabbit; *rabbit = tmp;
+            assert(hole == first);
+            tmp = *hole; *hole = *rabbit; *rabbit = tmp;
             anchor = hole;
         }
-        else if (anchor == NULL) break;	// no changed
+        else if (anchor == NULL) break; // no changed
         first = anchor + 1;
     } while (first < last);
 #ifdef DEBUG
