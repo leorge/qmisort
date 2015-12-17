225cc5870edfa28cd8331048bdc7d3bf042bd966
leorge <qmisort@gmail.com>
2015-12-18 06:02:57 +0900

    Add cocktail_sort()

--- bubble_sort.c
+++ bubble_sort.c
@@ -16,7 +16,7 @@ void bubble_sort(void **base, size_t nmemb, int (*compare)(const void *, const v
 #endif
     void **last = base + (nmemb - 1);
     do {
-        void **p1, **p2, **anchor = base + 1;
+        void **p1, **p2, **anchor = NULL;
 #ifdef DEBUG
         dump_pointer("search", base, last - base + 1);
         qsort_called++; // loop count
@@ -26,14 +26,56 @@ void bubble_sort(void **base, size_t nmemb, int (*compare)(const void *, const v
 #ifdef DEBUG
 				if (trace_level >= TRACE_MOVE) fprintf(OUT, "%s <--> %s\n", dump_data(*p1), dump_data(*p2));
 #endif
-				void *tmp = *p1; *p1 = *p2; *p2 = tmp;	// swap
-				anchor = p2;
+				void *tmp = *p1; *(anchor = p1) = *p2; *p2 = tmp;	// swap
             }
         }
-        last = anchor - 1;
+        if (anchor == NULL) break;
+        last = anchor;
     } while (base < last);
 #ifdef DEBUG
     dump_pointer("bubble_sort() done.", base, nmemb);
+#endif
+}
+
+void cocktail_sort(void **base, size_t nmemb, int (*compare)(const void *, const void *)) {
+    if (nmemb <= 1) return;
+#ifdef DEBUG
+    dump_pointer("cocktail_sort() start in " __FILE__, base, nmemb);
+#endif
+    void **last = base + (nmemb - 1);
+    do {
+        void **p1, **p2, **anchor = NULL;
+#ifdef DEBUG
+        dump_pointer("search  forward", base, last - base + 1);
+        qsort_called++; // loop count
+#endif
+        for (p1 = base; p1 < last; p1 = p2) {   // search a reverse order pair
+            if (compare(*p1, *(p2 = p1 + 1)) > 0) {  // found
+#ifdef DEBUG
+				if (trace_level >= TRACE_MOVE) fprintf(OUT, "%s <--> %s\n", dump_data(*p1), dump_data(*p2));
+#endif
+				void *tmp = *p1; *(anchor = p1) = *p2; *p2 = tmp;	// swap
+            }
+        }
+        if (anchor == NULL) break;
+        last = anchor;
+#ifdef DEBUG
+        dump_pointer("search backward", base, last - base + 1);
+        qsort_called++; // loop count
+#endif
+        for (p2 = last; p2 > base; p2 = p1) {   // search a reverse order pair
+            if (compare(*(p1 = p2 - 1), *p2) > 0) {  // found
+#ifdef DEBUG
+				if (trace_level >= TRACE_MOVE) fprintf(OUT, "%s <--> %s\n", dump_data(*p1), dump_data(*p2));
+#endif
+				void *tmp = *p1; *p1 = *p2; *(anchor = p2) = tmp;	// swap
+            }
+        }
+        if (anchor == NULL) break;
+        base = anchor;
+    } while (base < last);
+#ifdef DEBUG
+    dump_pointer("cocktail_sort() done.", base, nmemb);
 #endif
 }
 
