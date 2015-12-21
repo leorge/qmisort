c81f1afee081296b88990a506cdb3e5635d42cdd
leorge <qmisort@gmail.com>
2015-12-21 21:19:00 +0900

    merge rabbit_sort() into bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -79,6 +79,82 @@ void cocktail_sort(void **base, size_t nmemb, int (*compare)(const void *, const
 #endif
 }
 
+void rabbit_sort(void **base, size_t nmemb, int (*compare)(const void *, const void *)) {
+    if (nmemb <= 1) return;
+#ifdef DEBUG
+    dump_pointer("rabbit_sort() start in " __FILE__, base, nmemb);
+#endif
+    char **first = base, **last = first + nmemb - 1;
+    do {
+#ifdef DEBUG
+        dump_pointer("Search a greater rabbit.", first, last - first + 1);
+        qsort_called++; // corresponds to recursive calls
+#endif
+        char **rabbit, *tmp;
+        char **hole = NULL;     // a rabbit hole
+        char **anchor = NULL;   // exchanged position
+        for (char **p = rabbit = first; p++ < last;) {
+            int chk = compare(*p, *rabbit); // compare the size
+            if (chk < 0) hole = p;  // a smaller rabbit is in a hole.
+            else {  // found a not smaller rabbit
+                if (hole != NULL) {
+#ifdef DEBUG
+                	if (trace_level >= TRACE_MOVE)
+                		fprintf(OUT, "%s <--> %s\n", dump_data(*rabbit), dump_data(*hole));
+#endif
+                    tmp = *hole; *hole = *rabbit; *rabbit = tmp;  // push out a smaller rabbit.
+                    anchor = hole; hole = NULL; rabbit = p;
+                }
+                else if (chk > 0) rabbit = p;   // a bigger rabbit.
+            }
+        }
+        if (hole != NULL) { // The biggest rabbit is roaming.
+            assert(hole == last);
+#ifdef DEBUG
+			if (trace_level >= TRACE_MOVE)
+				fprintf(OUT, "%s <--> %s\n", dump_data(*rabbit), dump_data(*hole));
+#endif
+            tmp = *hole; *hole = *rabbit; *rabbit = tmp;
+            anchor = hole; hole = NULL;
+        }
+        else if (anchor == NULL) break; // no changed
+        last = anchor - 1;
+#ifdef DEBUG
+        dump_pointer("Search a lesser  rabbit.", first, last - first + 1);
+        qsort_called++; // corresponds to recursive calls
+#endif
+        for (char **p = rabbit = last; p-- > first;) {
+            int chk = compare(*p, *rabbit);
+            if (chk > 0) hole = p;
+            else {
+                if (hole != NULL) {
+#ifdef DEBUG
+                	if (trace_level >= TRACE_MOVE)
+                		fprintf(OUT, "%s <--> %s\n", dump_data(*rabbit), dump_data(*hole));
+#endif
+                    tmp = *hole; *hole = *rabbit; *rabbit = tmp;
+                    anchor = hole; hole = NULL; rabbit = p;
+                }
+                else if (chk < 0) rabbit = p;
+            }
+        }
+        if (hole != NULL) {
+            assert(hole == first);
+#ifdef DEBUG
+			if (trace_level >= TRACE_MOVE)
+				fprintf(OUT, "%s <--> %s\n", dump_data(*rabbit), dump_data(*hole));
+#endif
+            tmp = *hole; *hole = *rabbit; *rabbit = tmp;
+            anchor = hole;
+        }
+        else if (anchor == NULL) break; // no changed
+        first = anchor + 1;
+    } while (first < last);
+#ifdef DEBUG
+    dump_pointer("rabbit_sort() done.", base, nmemb);
+#endif
+}
+
 void comb_sort(void **base, size_t nmemb, int (*compare)(const void *, const void *)) {
     if (nmemb <= 1) return;
 #ifdef DEBUG
