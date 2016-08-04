a5e73504c510ab45e7ee1d6152bda5b5dc4df3b1
leorge <qmisort@gmail.com>
2016-08-05 06:06:19 +0900

    Common subroutine to choose the median of serveral elements.

--- get_pivot.c
+++ get_pivot.c
@@ -1,31 +1,30 @@
 /*
  * get_pivot.c
  *
- *  Search the median of several elements.
+ *  Choose the median of several elements.
  *
  *  Created on: 2015/05/27
  *      Author: leo
  */
 #include    "sort.h"
 
-void *median_of_5(void *base, size_t nmemb, size_t size, int (*compare)(const void *, const void *), size_t random) {
+void *median_of_5(void *base, size_t nmemb, size_t size, int (*compare)(const void *, const void *)) {
     void *rtn;
-    size_t distance = (nmemb >> 3) + (nmemb >> 4);  // N/8 + N/16 = 3N/16
     char *p1, *p2, *p3, *p4, *p5;
-    p1 = (char *)base + ((distance * random) / RAND_BASE) * size; // in [0, 3N/16)
-    distance *= size;
-    p5 = (p4 = (p3 = (p2 = p1 + distance) + distance) + distance) + distance;   // [3N/16, 15N/16)
+    p1 = (char *)base + (((nmemb >> 2) * rand()) / RAND_BASE) * size; // [0, N/4)
+    size_t distance = ((nmemb >> 3) + (nmemb >> 4)) * size;  // N/8 + N/16 = 3N/16
+    p5 = (p4 = (p3 = (p2 = p1 + distance) + distance) + distance) + distance;   // [3N/16, N)
 #ifdef  DEBUG
     if (trace_level >= TRACE_DUMP) fprintf(OUT, "Median of 5 in %ld from %s %s %s %s %s",
         nmemb, dump_data(p1), dump_data(p2), dump_data(p3), dump_data(p4), dump_data(p5));
 #endif
-    // If you are a librarian, you can convert If statements to ternary operators.
-    if (compare(p1, p5) > 0) {char *tmp = p1; p1 = p5; p5 = tmp;}   // p1 <--> p5 then *p1 < *p5
+    // You can rewrite statements below with a plenty of ternary operators.
     if (compare(p2, p4) > 0) {char *tmp = p2; p2 = p4; p4 = tmp;}   // p2 <--> P4 then *p2 < *p4
-    if (compare(p3, p2) < 0) {char *tmp = p2; p2 = p3; p3 = tmp;}   // p3 <--> p2 then *p2 < *p3 < *p4
-    else if (compare(p4, p3) < 0) {char *tmp = p4; p4 = p3; p3 = tmp;}  // p4 <--> p3 then *p2 < *p3 < *p4
-    rtn = compare(p3, p1) < 0 ? (compare(p1, p4) < 0 ? p1 : p4)
-                            : (compare(p5, p3) < 0 ? (compare(p5, p2) < 0 ? p2 : p5) : p3);
+    if (compare(p2, p3) > 0) {char *tmp = p2; p2 = p3; p3 = tmp;}   // p3 <--> p2 then *p2 < *p3 < *p4
+    else if (compare(p3, p4) > 0) {char *tmp = p4; p4 = p3; p3 = tmp;}  // p4 <--> p3 then *p2 < *p3 < *p4
+    if (compare(p1, p5) > 0) {char *tmp = p1; p1 = p5; p5 = tmp;}   // p1 <--> p5 then *p1 < *p5
+    rtn =  compare(p3, p1) < 0 ? (compare(p1, p4) < 0 ? p1 : p4)
+        : (compare(p5, p3) < 0 ? (compare(p5, p2) < 0 ? p2 : p5) : p3);
 #ifdef  DEBUG
     if (trace_level >= TRACE_DUMP) fprintf(OUT, " --> %s\n", dump_data(rtn));
     int LEFT = 0, RIGHT=0, CHK;
@@ -40,41 +39,41 @@ static void *search_median(void **base, size_t nmemb, int (*compare)(const void 
 {
 #ifdef DEBUG
     search_pivot++;
-    if (trace_level >= TRACE_DUMP) fprintf(OUT, "search_median() : ");
+    if (trace_level >= TRACE_DUMP) dump_pointer("search_median() : ", base, nmemb);
     assert(base != NULL);
     assert(nmemb != 0);
     assert(compare != NULL);
 #endif
-    void **top = base, **bottom = &base[nmemb - 1], **middle = &base[nmemb >> 1];
-    while (top < bottom) {
+    void **left = base, **right = &base[nmemb - 1], **middle = &base[nmemb >> 1];
+    while (left < right) {
 #ifdef  DEBUG
-        dump_pointer("array", top, bottom - top + 1);
+        dump_pointer("array", left, right - left + 1);
 #endif
-        void **hole = &top[(bottom - top) >> 1];    // middle
-        void *pivot = *hole;    // store middle data
-        *hole = *bottom;
+        void **hole = &left[(right - left) >> 1];
+        void *pivot = *hole;    // save middle data
+        *hole = *right;         // last data --> middle
 #ifdef  DEBUG
         if (trace_level >= TRACE_DUMP) fprintf(OUT, "pivot <-- %s <-- %s\n"
                 , dump_data(pivot), dump_data(*hole));
 #endif
-        hole = bottom; // last data --> middle
-        void **lo = top, **hi = bottom - 1, **eq = NULL;
-        for (; lo < hole; lo++) {
-            if (compare(*lo, pivot) >= 0) {
+        hole = right;           // dig a hole at the last
+        void **lo = left, **hi = right - 1, **eq = NULL;
+        for (int chk; lo < hole; lo++) {
+            if ((chk = compare(*lo, pivot))>= 0) {
 #ifdef  DEBUG
                 if (trace_level >= TRACE_MOVE) fprintf(OUT, "move %s --> %s\n"
                         , dump_data(*lo), dump_data(*hole));
 #endif
+                if (chk > 0) eq = NULL; // discontinued
+                else if (eq == NULL) eq = hole;
                 *hole = *lo; hole = lo;
                 for (; hi > hole; hi--) {
-                    int chk;
-                    if ((chk =compare(*hi, pivot)) < 0) {
+                    if ((chk = compare(*hi, pivot)) < 0) {
 #ifdef  DEBUG
                         if (trace_level >= TRACE_MOVE) fprintf(OUT, "move %s <-- %s\n"
                                 , dump_data(*hole), dump_data(*hi));
 #endif
                         *hole = *hi; hole = hi;
-                        eq = NULL;  // not equal then reset
                     }
                     else if (chk > 0) eq = NULL;
                     else if (eq == NULL) eq = hi;   // first equal element
@@ -86,27 +85,29 @@ static void *search_median(void **base, size_t nmemb, int (*compare)(const void 
         if (trace_level >= TRACE_DUMP) {
             fprintf(OUT, "hole = %s\tmiddle = %s\teq = %s\n", dump_data(*hole),
                     dump_data(*middle), eq ? dump_data(*eq) : "(null)");
-            dump_pointer("", top, bottom - top + 1);
+            dump_pointer("", left, right - left + 1);
         }
 #endif
-        if (hole == middle) break;  // if input data is sorted then break soon.
-        else if (middle < hole) bottom = hole - 1;
-        else if (eq == NULL) top = hole + 1;    // hole < middle && not continuous
-        else if (eq < middle) top = eq + 1;     // hole < eq < middle
-        else break; // hole < middle <= eq
+        if (eq == NULL) eq = hole;  // phole <= peq
+        if (middle < hole) right = hole - 1;
+        else if (eq < middle) left = eq + 1;
+        else break;                 // phole <= middle <= peq
     }
 #ifdef DEBUG
-    if (trace_level >= TRACE_DUMP) fprintf(OUT,"search_median() --> %s\n", dump_data(*middle));
+    if (trace_level >= TRACE_DUMP) {
+        fprintf(OUT,"search_median() --> %s\n", dump_data(*middle));
+        dump_pointer("sorted array", base, nmemb);
+    }
 #endif
     return  *middle;
 }
 
 /* search a pivot in an array   */
-void *pivot_array(void *base, size_t nmemb, size_t size, size_t pickup, int (*compare)(const void *, const void *), size_t random)
+void *pivot_array(void *base, size_t nmemb, size_t size, size_t pickup, int (*compare)(const void *, const void *))
 {
 #ifdef  DEBUG
     if (trace_level >= TRACE_DUMP) fprintf(OUT,
-            "pivot_array(base, nmemb = %ld, size = %ld, pickup = %ld, compare(), random = %ld)\n", nmemb, size, pickup, random);
+            "pivot_array(base, nmemb = %ld, size = %ld, pickup = %ld, compare())\n", nmemb, size, pickup);
     assert(base != NULL);
     assert(nmemb > 1);
     assert(size != 0);
@@ -120,7 +121,7 @@ void *pivot_array(void *base, size_t nmemb, size_t size, size_t pickup, int (*co
 #endif
     distance *= size;     // size in byte
 #define first   ((char *)base)
-    char *p = first + (nmemb * random / RAND_BASE / pickup) * size;  // 1st pick up point
+    char *p = first + (nmemb * rand() / RAND_BASE / pickup) * size;  // 1st pick up point
 #ifdef  DEBUG
     char tmp[10], buffer[pickup * sizeof(tmp)]; *buffer = '\0';
 #endif
