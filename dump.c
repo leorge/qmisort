ec0c739e2d197daf8c00d1190d4e972be55fb2f9
leorge <qmisort@gmail.com>
2016-08-05 06:04:17 +0900

    Dump data for debugging.

--- dump.c
+++ dump.c
@@ -5,37 +5,40 @@
  *      Author: leo
  */
 
-#ifdef DEBUG
-
 #include    "sort.h"
-
-bool    ispointer = FALSE;
 
 // Dump an element.
 const char *dump_data(const void *data) {
-    return ispointer? *(const char **)data: data ;
+    return data;
 }
 
 void dump_copy(void *dst, const void *src)
 {
-qsort_moved++;
-if (trace_level >= TRACE_MOVE) fprintf(OUT, "copy(dst = %s, src = %s)\n", dump_data(dst), dump_data(src));
+    qsort_moved++;
+    if (trace_level >= TRACE_MOVE) fprintf(OUT, "copy(dst = %s, src = %s)\n", dump_data(dst), dump_data(src));
 }
 
-void dump_array(const char *msg, const void *base, size_t nmemb, size_t size)
+void dump_array(const char *msg, const void *base, size_t left, size_t middle, size_t right, size_t size)
 {
     if (trace_level < TRACE_DUMP) return;
-    size_t  i;
+    size_t  i, nmemb = left + middle + right;
     const char  *p = base;
     fprintf(OUT, "%s nmemb = %ld :", msg, nmemb);
-    if (nmemb > 16) {
+    if (nmemb <= 16) {
+        if (left > 0) for (i = 0; i++ < left; p += size) fprintf(OUT, " %s", dump_data(p));
+        if (middle > 0 || right > 0) {
+            fprintf(OUT, " [");
+            for(i = 0; i++ < middle; p+= size) fprintf(OUT, " %s", dump_data(p));
+            fprintf(OUT, " ]");
+        }
+        if (right > 0) for (i = 0; i++ < right; p += size) fprintf(OUT, " %s", dump_data(p));
+    } else {
         for (i = 0; i++ < 8; p += size) fprintf(OUT, " %s", dump_data(p));
         fprintf(OUT, " ...");
         p = base;
         p += size * (i = nmemb - 8);
         for (; i++ < nmemb; p += size) fprintf(OUT, " %s", dump_data(p));
     }
-    else for (i = 0; i++ < nmemb; p += size) fprintf(OUT, " %s", dump_data(p));
     fprintf(OUT, "\n");
 }
 
@@ -53,25 +56,9 @@ void dump_pointer(char *msg, void *base[], size_t nmemb) {
 //  if (nmemb > 16) dump_pointer(msg, &base[8], nmemb - 16);    // dump whole data
 }
 
-char *dump_index(char *buf, INSERT_INDEX index, int length) {
-    if (trace_level < TRACE_DUMP) return NULL;
-    char    *p = buf;
-    int     i;
-    for (i = 0; i++ < length; ) *p++ = '\0';    // fill with zero
-    *p = '\0';
-    for (i = 0; i++ < length;) {
-        int c = index & MASK_DIGIT;
-        *--p = c + (c < 10 ? '0' : 'A' - 10);   // Hexadecimal character
-        index >>= DIGIT_WIDTH;
-        index &= ((INSERT_INDEX)1 << ((MAX_SIZE - 1) * DIGIT_WIDTH)) - 1;
-    }
-    return  buf;
-}
-
 void    dump_rate(size_t n_lo, size_t n_hi) {
     if (trace_level >= TRACE_DUMP)
         fprintf(OUT, "sub partition ( %ld , %ld ) rate = %.3f\tdiff = %ld\n", n_lo, n_hi
             , (double)n_lo / (n_lo + n_hi)
             , (n_lo > n_hi)? (n_lo - n_hi): (n_hi - n_lo));
 }
-#endif
