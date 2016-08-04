d5db7a1743f5223e7dcbeaaa8919b6ef1c428c6f
leorge <qmisort@gmail.com>
2016-08-05 06:35:04 +0900

    linear/binary insertion sort, accelerated insertion sort and shell sort.

--- insert_sort.c
+++ insert_sort.c
@@ -1,7 +1,7 @@
 /*
  * insert_linear.c
  *
- *  Insertion sort with linear search.
+ *  Several insertion sort.
  *
  *  Created on: 2013/01/08
  *      Author: leo
@@ -9,11 +9,18 @@
 
 #include "sort.h"
 
+/* nibble sort is one of the binary insertion sort */
+
+#define DIGIT_WIDTH 4       // nibble
+#define MAX_SIZE    (1 << DIGIT_WIDTH)
+#define MASK_DIGIT  (MAX_SIZE - 1)
+typedef unsigned long long INSERT_INDEX;    // 4 bits/element * 16 elements = 64 bits = bit width of long long
+
 /* binary search */
 void insert_binary(void **base, size_t nmemb, int (*compare)(const void *, const void *)) {
     if (nmemb <= 1) return;
 #ifdef DEBUG
-    dump_pointer("bins_pointer() start in " __FILE__, base, nmemb);
+    dump_pointer("insert_binary() start in " __FILE__, base, nmemb);
 #endif
     void **hole = base;
     for (size_t idx = 1; idx < nmemb; idx++) {
@@ -42,7 +49,7 @@ void insert_binary(void **base, size_t nmemb, int (*compare)(const void *, const
 #endif
 }
 
-/* linear search */
+/* linear search is the final stage of shell sort */
 void insert_linear(void **base, size_t nmemb, int (*compare)(const void *, const void *)) {
     if (nmemb <= 1) return;
 #ifdef DEBUG
@@ -54,15 +61,15 @@ void insert_linear(void **base, size_t nmemb, int (*compare)(const void *, const
         qsort_called++;
         dump_pointer("", base, hole - base);
 #endif
-    	void **p1, **p2, *pivot = *hole;	// make a hole
-    	for (p2 = hole; (p1 = p2 - 1) >= base; p2 = p1) {
-    		if (compare(*p1, pivot) <= 0) break;
-    		*p2 = *p1;
-    	}
-    	*p2 = pivot;
+        void **p1, **p2, *pivot = *hole;    // make a hole
+        for (p2 = hole; (p1 = p2 - 1) >= base; p2 = p1) {
+            if (compare(*p1, pivot) <= 0) break;
+            *p2 = *p1;
+        }
+        *p2 = pivot;
     }
 #ifdef DEBUG
-    dump_pointer("insert_sort() done.", base, nmemb);
+    dump_pointer("insert_linear() done.", base, nmemb);
 #endif
 }
 
@@ -75,16 +82,16 @@ void ai_sort(void **base, size_t nmemb, int (*compare)(const void *, const void 
 #endif
     void **last = &base[nmemb - 1];     // point the last element
     for (void **p1 = base, **p2 = last; p1 < last;) {
-    	if (compare(*p1, *p2) > 0){
+        if (compare(*p1, *p2) > 0){
 #ifdef DEBUG
-    	    if (trace_level >= TRACE_DUMP)
-    	    	fprintf(OUT, "ai_sort() swap %s <--> %s\n", dump_data(*p1), dump_data(*p2));
+            if (trace_level >= TRACE_DUMP)
+                fprintf(OUT, "ai_sort() swap %s <--> %s\n", dump_data(*p1), dump_data(*p2));
 #endif
-    		void *tmp = *p1; *p1 = *p2; *p2 = tmp;	// swap
-    	}
-    	p1++;
-    	if (++p2 > last)
-    		p2 = p1 + ((p2 - p1) >> 1);
+            void *tmp = *p1; *p1 = *p2; *p2 = tmp;  // swap
+        }
+        p1++;
+        if (++p2 > last)
+            p2 = p1 + ((p2 - p1) >> 1);
     }
     insert_linear(base, nmemb - 1, compare);
 }
@@ -95,32 +102,32 @@ void shell_sort(void **base, size_t nmemb, int (*compare)(const void *, const vo
 #ifdef DEBUG
     dump_pointer("shell_sort() start in " __FILE__, base, nmemb);
     if (trace_level >= TRACE_DUMP) {
-    	fprintf(OUT, "gaplist =");
+        fprintf(OUT, "gaplist =");
         for (int gap = 0; gap < gap_count; gap++) fprintf(OUT, " %ld", gaplist[gap]);
-    	fprintf(OUT, "\n");
+        fprintf(OUT, "\n");
     }
 #endif
     void **last = &base[nmemb - 1];     // point the last element
-    size_t	*gap_pointer = gaplist;
+    size_t  *gap_pointer = gaplist;
     for (int i = 0; i < gap_count; i++) {
-    	size_t	gap = *gap_pointer++;
+        size_t  gap = *gap_pointer++;
 #ifdef DEBUG
-    	if (trace_level >= TRACE_DUMP) {
-    		fprintf(OUT, "gap = %ld", gap);
-    		dump_pointer("", base, nmemb);
-    	}
+        if (trace_level >= TRACE_DUMP) {
+            fprintf(OUT, "gap = %ld", gap);
+            dump_pointer("", base, nmemb);
+        }
 #endif
-		for (void **hole = base + gap; hole <= last; hole++) {
+        for (void **hole = base + gap; hole <= last; hole++) {
 #ifdef DEBUG
-			qsort_called++;
+            qsort_called++;
 #endif
-			void **p1, **p2, *pivot = *hole;	// make a hole
-			for (p2 = hole; (p1 = p2 - gap) >= base; p2 = p1) {
-				if (compare(*p1, pivot) <= 0) break;
-				*p2 = *p1;
-			}
-			*p2 = pivot;
-		}
+            void **p1, **p2, *pivot = *hole;    // make a hole
+            for (p2 = hole; (p1 = p2 - gap) >= base; p2 = p1) {
+                if (compare(*p1, pivot) <= 0) break;
+                *p2 = *p1;
+            }
+            *p2 = pivot;
+        }
     }
 #ifdef DEBUG
     dump_pointer("shell_sort() done.", base, nmemb);
