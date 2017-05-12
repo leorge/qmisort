ea8c14c8f3df3cfe7de4c9d29f1a580414e73efb
leorge <qmisort@gmail.com>
2017-05-12 10:05:44 +0900

    %d or %ld for size_t

--- dump.c
+++ dump.c
@@ -7,6 +7,13 @@
 
 #include    "sort.h"
 
+static char *dump_size_t(size_t val) {
+	static char	buf[32];
+	static const char *fmt = sizeof(size_t)==sizeof(unsigned int)? "%d": "%ld";
+	sprintf(buf, fmt, val);
+	return	buf;
+}
+
 // Dump an element.
 const char *dump_data(const void *data) {
     return data;
@@ -23,7 +30,7 @@ void dump_array(const char *msg, const void *base, size_t left, size_t middle, s
     if (trace_level < TRACE_DUMP) return;
     size_t  i, nmemb = left + middle + right;
     const char  *p = base;
-    fprintf(OUT, "%s nmemb = %ld :", msg, nmemb);
+    fprintf(OUT, "%s nmemb = %s :", msg, dump_size_t(nmemb));
     if (nmemb <= 16) {
         if (left > 0) for (i = 0; i++ < left; p += size) fprintf(OUT, " %s", dump_data(p));
         if (middle > 0 || right > 0) {
@@ -45,7 +52,7 @@ void dump_array(const char *msg, const void *base, size_t left, size_t middle, s
 void dump_pointer(char *msg, void *base[], size_t nmemb) {
     if (trace_level < TRACE_DUMP) return;
     size_t  i;
-    fprintf(OUT, "%s nmemb = %ld :", msg, nmemb);
+    fprintf(OUT, "%s nmemb = %s :", msg, dump_size_t(nmemb));
     if (nmemb > 16) {
         for (i = 0; i < 8;) fprintf(OUT, " %s", dump_data((char *)base[i++]));
         fprintf(OUT, " ...");
@@ -58,7 +65,7 @@ void dump_pointer(char *msg, void *base[], size_t nmemb) {
 
 void    dump_rate(size_t n_lo, size_t n_hi) {
     if (trace_level >= TRACE_DUMP)
-        fprintf(OUT, "sub partition ( %ld , %ld ) rate = %.3f\tdiff = %ld\n", n_lo, n_hi
-            , (double)n_lo / (n_lo + n_hi)
-            , (n_lo > n_hi)? (n_lo - n_hi): (n_hi - n_lo));
+        fprintf(OUT, "sub partition ( %s , %s ) rate = %.3f\tdiff = %s\n"
+        	, dump_size_t(n_lo), dump_size_t(n_hi), (double)n_lo / (n_lo + n_hi)
+            , (n_lo > n_hi)? dump_size_t(n_lo - n_hi): dump_size_t(n_hi - n_lo));
 }
